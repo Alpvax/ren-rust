8389035d5ba93d30d3c5ced80091ba3a2cdbc099
Alpvax <development@alpvax.uk>
2022-04-08 12:45:05 +0100

    [TEMP]Begin declaration parsing

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -3,6 +3,9 @@ pub(crate) type Token = lexer::Token;
 pub(crate) mod import;
 pub use import::parse_import;
 
+pub(crate) mod declaration;
+pub use declaration::{parse_block_declaration, parse_module_declaration};
+
 // pub type ParserFunc<'source, T/*, P: TryInto<T>*/> = fn(&'source mut lexer::Lexer<'source, Token>) -> T;
 
 #[cfg(test)]
